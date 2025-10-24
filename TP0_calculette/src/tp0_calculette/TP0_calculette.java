5b62bd57b7cbe22333bc07ad11fadeef7537132e
Titouan HETMANIAK <Titouan HETMANIAK@10.3.1.218>
2025-10-24 12:58:31 +0200

    TP0_ calculette en cours

---
+++ TP0_calculette/src/tp0_calculette/TP0_calculette.java
@@ -0,0 +1,52 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package tp0_calculette;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Titouan HETMANIAK
+ */
+public class TP0_calculette {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        int operateur;
+        int operande1;
+        int operande2;
+        int resultat = 0; 
+        
+        System.out.println("Please enter the operator:");
+        System.out.println("1) add");
+        System.out.println("2) substract");
+        System.out.println("3) multiply");
+        System.out.println("4) divide");
+        System.out.println("5) modulo");
+        
+        operateur = sc.nextInt(); 
+        
+         if (operateur < 1 || operateur > 5) {
+            System.out.println("Erreur : opérateur invalide !");
+         } else {
+             
+              System.out.print("Please enter the first number: ");
+              operande1 = sc.nextInt();
+         }
+              System.out.print("Please enter the second number: ");
+              operande2 = sc.nextInt();
+              
+              if (operateur == 1) {
+                  resultat = operande1 + operande2;
+               } else if (operateur == 2) {
+                   resultat = operande1 - operande2;
+                
+    }
+    
+}
