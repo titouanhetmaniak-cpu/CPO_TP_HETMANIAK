b729477b86f1714c5b5c6769da9473afe211dc96
Titouan HETMANIAK <Titouan HETMANIAK@10.3.1.218>
2025-12-15 14:49:10 +0100

    TP3_heroic fantasy FINI

---
+++ TP3_Heroic_Fantasy_Hetmaniak/src/tp3_heroic_fantasy_hetmaniak/TP3_Heroic_Fantasy_Hetmaniak.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package tp3_heroic_fantasy_hetmaniak;
+
+/**
+ *
+ * @author Titouan HETMANIAK
+ */
+public class TP3_Heroic_Fantasy_Hetmaniak {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
